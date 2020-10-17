c4dc998693299ee578e20ffbadaa5b33efacc580
easternWolf9222 <53422996+easternWolf9222@users.noreply.github.com>
2020-10-17 20:49:56 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,19 +11,23 @@
 using namespace std;
 
 int main() {
-    float x1, y1, x2, y2;
+    int x1, y1, x2, y2;
     while(cin >> x1 >> y1 >> x2 >> y2){
-        if(x1 == x2 && y1 == y2){
+        
+        if(x1 == x2 && y1 == y2){ //兩人在同一點上
             cout << 1 << endl;
-        }else if(x1 == x2){
+            
+        }else if(x1 == x2){ //x座標一樣
             cout << abs(y1-y2) + 1 << endl;
-        }else if(y1 == y2){
+            
+        }else if(y1 == y2){ //y座標一樣
             cout << abs(x1-x2) + 1 << endl;
+            
         }else{
             int a = abs(x1-x2);
             int b = abs(y1-y2);
             int t;
-            while(b != 0)
+            while(b != 0) //做輾轉相除，得最大公因數
             {
                 t = b;
                 b = a%b;
