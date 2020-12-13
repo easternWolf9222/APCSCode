5cf01185b2a91d0178ab15cb485d807bd7a3d631
easternWolf9222 <53422996+easternWolf9222@users.noreply.github.com>
2020-12-13 23:05:51 +0800

    最大連續子陣列

---
+++ 例題P-4-13 2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n, a[100010], maxSum = 0, tmp = 0;
+    cin >> n;
+    for(int i = 0 ; i < n ; i++){
+        cin >> a[i];
+    }
+
+    for(int i = 0 ; i < n ; i++){
+        if(tmp <= 0)
+            tmp = a[i];
+        else
+            tmp += a[i];
+        if(tmp > maxSum)
+            maxSum = tmp;
+    }
+
+    cout << maxSum << endl;
+
+    return 0;
+}
